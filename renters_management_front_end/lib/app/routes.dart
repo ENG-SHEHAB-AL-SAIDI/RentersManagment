abf092727fbcc286f3f8bef831a74dcfa1f9cddc
SHEHAB <shshehab02@gmail.com>
2024-07-12 02:13:13 +0300

    create login view

--- renters_management_front_end/lib/app/routes.dart
+++ renters_management_front_end/lib/app/routes.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
-import './views/login_view.dart';
+import 'package:renters_management_front_end/app/views/login_view/main_login_view.dart';
+import 'bindings/login_binding.dart';
 
 class AppRoutes {
   static final routes = [
     GetPage(
       name: '/login',
-      page: ()=> const LoginView()
+      page: ()=>  const LoginPage(),
+      binding: LoginViewBinding(),
     ),
     // Add more routes here
   ];
---
+++ renters_management_front_end/lib/app/views/login_view/main_login_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import './phones_login_view.dart';
+import './web_login_view.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+    return Material(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth <= 768 && constraints.maxHeight <= 1025) {
+            return  PhoneLoginView();
+          } else {
+            return  WebLoginView();
+          }
+        },
+      ),
+    );
+  }
+}

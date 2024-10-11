056498c3b6e5038b9ddcc59e48e9e9f513c18daa
SHEHAB <shshehab02@gmail.com>
2024-10-11 05:14:38 +0300

    update installment seeder to update payment fildes after add installment

--- renters_management_back_end/database/seeders/RentPaymentsInstallmentSeeder.php
+++ renters_management_back_end/database/seeders/RentPaymentsInstallmentSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\RentPayment;
 use App\Models\RentPaymentsInstallment;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class RentPaymentsInstallmentSeeder extends Seeder
@@ -15,18 +14,31 @@ class RentPaymentsInstallmentSeeder extends Seeder
     protected int $count;
     protected RentPayment $rentPayment;
 
-    public function __construct(int $count = null, RentPayment $rentPayment) {
+    public function __construct(int $count = null, RentPayment $rentPayment)
+    {
         $this->count = $count;
         $this->rentPayment = $rentPayment;
     }
 
     public function run(): void
     {
-        $rent = $this->rentPayment->renter->rent;
-        RentPaymentsInstallment::factory($this->count??1)->for($this->rentPayment)->create(
-            [
-                'amount'=>fake()->randomFloat(null,0,$rent),
-            ]
-        );
+        for ($i = 0; $i < $this->count; $i++) {
+
+            $installment = RentPaymentsInstallment::factory()->for($this->rentPayment)->create(
+                [
+                    'amount' => fake()->randomFloat(null, 0, $this->rentPayment->remain_amount),
+                ]
+            );
+            $this->rentPayment->remain_amount -= $installment->amount;
+            $this->rentPayment->payed_amount += $installment->amount;
+            if ($this->rentPayment->payed_amount == 0) {
+                $this->rentPayment->state = 'not_payed';
+            } elseif ($this->rentPayment->remain_amount == 0) {
+                $this->rentPayment->state = 'payed';
+            } else {
+                $this->rentPayment->state = 'partially_payed';
+            }
+            $this->rentPayment->save();
+        }
     }
 }

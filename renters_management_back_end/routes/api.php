b6f4a87ae3fceef17db693f655a5b5a431ca5944
SHEHAB <shshehab02@gmail.com>
2024-10-31 03:03:44 +0300

    add yearcontroller

--- renters_management_back_end/routes/api.php
+++ renters_management_back_end/routes/api.php
@@ -2,33 +2,36 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BuildController;
-use App\Http\Controllers\CombinDataController;
 use App\Http\Controllers\RenterController;
 use App\Http\Controllers\RentPaymentController;
-use App\Http\Controllers\RentPaymentsInstallmentController;
-use App\Models\RentPayment;
+use App\Http\Controllers\YearController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api'])->prefix('auth')->group(
-function ($router) {
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:api');
-    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
-    Route::post('/me', [AuthController::class, 'me'])->name('me')->middleware('auth:api');
-});
+    function ($router) {
+        Route::post('/register', [AuthController::class, 'register'])->name('register');
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:api');
+        Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+        Route::post('/me', [AuthController::class, 'me'])->name('me')->middleware('auth:api');
+    }
+);
 
-Route::middleware(['api','auth:api'])->prefix('user')->group(
-function () {
-    Route::apiResource('builds',BuildController::class);
-    Route::apiResource('builds.renters',RenterController::class);
-    Route::post('renters/{id}',[RenterController::class,'addPhone'])->name('renters.addPhone');
-    Route::delete('renters/{id}',[RenterController::class,'destroyPhone'])->name('renters.deletePhone');
-    Route::apiResource('renters.rent_payments',RentPaymentController::class);
-    Route::post('rent_payments/{id}/installments',[RentPaymentController::class,'addInstallment'])->name('rent_payments.addInstallment');
-    Route::delete('rent_payments/{id}/installments/{installmentId}',[RentPaymentController::class,'deleteInstallment'])->name('rent_payments.deleteInstallment');
+Route::middleware(['api', 'auth:api'])->prefix('user')->group(
+    function () {
+        Route::apiResource('builds', BuildController::class);
+        Route::apiResource('builds.renters', RenterController::class);
 
+        Route::post('renters/{id}/phones', [RenterController::class, 'addPhone'])->name('renters.addPhone');
+        Route::delete('renters/{id}/phones', [RenterController::class, 'destroyPhone'])->name('renters.deletePhone');
+        Route::apiResource('renters.rent_payments', RentPaymentController::class);
 
+        Route::post('rent_payments/{id}/installments', [RentPaymentController::class, 'addInstallment'])->name('rent_payments.addInstallment');
+        Route::delete('rent_payments/{id}/installments/{installmentId}', [RentPaymentController::class, 'deleteInstallment'])->name('rent_payments.deleteInstallment');
 
-    // Route::get('getAllData',[CombinDataController::class, 'getAllUserData']);
-});
+        Route::post('year/renters/{renterId}',[YearController::class,'addYearToRenter'])->name('addYearToRenter');
+        Route::delete('year/renters/{renterId}',[YearController::class,'deleteYearToRenter'])->name('deleteYearToRenter');
+        
+        Route::post('year/builds/{renterId}', [YearController::class, 'addYearToBuild'])->name('addYearToBuild');
+    }
+);

56fc7e39b4a1854056187c2e724d508ecf393761
SHEHAB <shshehab02@gmail.com>
2025-07-12 00:08:36 +0300

    solve login screen issuse on ios devices(web)

--- renters_management_front_end/lib/main.dart
+++ renters_management_front_end/lib/main.dart
@@ -18,19 +18,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetPlatform.isIOS
-        //IOS UI
-        ? GetCupertinoApp(
-            title: "Renters Management",
-            initialRoute: "/login",
-            translations: Languages(),
-            locale: Get.deviceLocale,
-            fallbackLocale: const Locale('en'),
-            getPages: AppRoutes.routes,
-            debugShowCheckedModeBanner: false,
-          )
-        // Android and web UI
-        : GetMaterialApp(
+    return  GetMaterialApp(
             title: "Renters Management",
             initialRoute: "/login",
             translations: Languages(),
